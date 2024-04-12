b061ad5a6f19bf1bb6057a412d632f54b0f13dd2
tomthelom <166326918+tomthelom@users.noreply.github.com>
2024-04-12 03:37:16 +0100

    Add files via upload

---
+++ public/sketch.js
@@ -0,0 +1,22 @@
+var socket = io({transports: ['websocket'], upgrade: false});
+
+function setup() {
+  createCanvas(400, 400);
+  background(220);
+
+  socket.on("info", spawn);
+}
+function spawn(data)
+{
+	fill(20,200,0);
+	ellipse(data.x,data.y,10,10);
+
+}
+
+function draw() {
+  
+  fill(200,0,100);
+	ellipse(mouseX,mouseY,10,10);
+	
+  socket.emit("info", {x:mouseX, y :mouseY});
+}
